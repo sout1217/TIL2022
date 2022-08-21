4b7b354e460a5d0f49e418cf42a110f5eadd9544
bh.hwang <bh.hwang@liveklass.com>
2022-08-21 14:42:29 +0900

    spring boot2 - jpa - 001. init

---
+++ spring_boot2/src/test/java/com/example/spring_boot2/SpringBoot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
